c916e7587563e88fe935f25d6d92e6bd6a2a8395
merkelvit-sys <merkelvit@googlemail.com>
2026-07-29 21:51:56 +0200

    Add files via upload

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,1 +1,6 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+// Register message listener on the top-level scope to satisfy Chrome's initial evaluation check
+self.addEventListener('message', function(event) {
+  // Handled by OneSignalSDK.sw.js
+});
