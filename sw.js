cc9d7bde9ee225bf358dcf5b36c739c2fded98ab
merkelvit-sys <merkelvit@googlemail.com>
2026-07-30 00:02:23 +0200

    fix: support SKIP_WAITING on update button click and reload automatically on controllerchange

--- sw.js
+++ sw.js
@@ -1,8 +1,11 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
 
 // Register message listener on the top-level scope to satisfy Chrome's initial evaluation check
-self.addEventListener('message', function(event) {
-  // Handled by OneSignalSDK.sw.js
+// and handle PWA SKIP_WAITING updates
+self.addEventListener('message', (event) => {
+  if (event.data && (event.data === 'SKIP_WAITING' || event.data.type === 'SKIP_WAITING')) {
+    self.skipWaiting();
+  }
 });
 
 const CACHE_NAME = 'service-schedule-v40'; // <-- v40: обновлено для новой кнопки-колокольчика и ее переключения вкл/выкл
