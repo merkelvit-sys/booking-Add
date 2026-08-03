39709796295efd6e526b068aba3fbd0a17859892
merkelvit-sys <merkelvit@googlemail.com>
2026-08-03 21:33:05 +0200

    fix: highlight current day (Today) prominently in week date selector pills and year view grid (v56)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('message', (event) => {
   }
 });
 
-const CACHE_NAME = 'service-schedule-v55';
+const CACHE_NAME = 'service-schedule-v56';
 const ASSETS = [
   './',
   './index.html',
