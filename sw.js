00786eac0dd3c2f6507af6087b2a98c6c1991d51
merkelvit-sys <merkelvit@googlemail.com>
2026-08-03 21:12:21 +0200

    feat: restrict YearScheduleMessage creation, editing, and deletion strictly to admin role and add delete button (v54)

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('message', (event) => {
   }
 });
 
-const CACHE_NAME = 'service-schedule-v53';
+const CACHE_NAME = 'service-schedule-v54';
 const ASSETS = [
   './',
   './index.html',
